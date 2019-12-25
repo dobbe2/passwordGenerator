845a363ba0d2a5af7d68e520560d11cbac4dd0a0
James Dobbe <dobbe2@gmail.com>
2019-12-25 14:32:00 -0800

    after much trial and error, I am starting to understand the loops and functions.  I am still working on getting code written to return the password using the users selected input types.  Have a session with a Tutor tomorrow to help with this.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is the 3rd assinment through bootcampspot for code boot camp.
 
+This application is designed to let the user have a random password created for them, using a variety of characters that the user can choose from.  The password can contain uppercase letter, lowercase letters, specail characters, and numbers. They can choose how long they would like their password as well, but it is desinged to be between 8-128 characters long.
+
 Password generator due 12/18/19
---
+++ script.js
@@ -0,0 +1,80 @@
+//setting vars
+  let lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let numbersChar = ["1","2","3","4","5","6","7","8","9","0"];
+  let specialChar = [ "!","#","$","%","&","(",")","*","+","-","/",":",";","<","=",">","?","@","[","^","_","`","{","|","}","~"];
+
+
+  //setting password length
+  function Length(){
+    var passwordLength = prompt("how long would you like your password? Must be between 8 and 128 characters")
+  if(passwordLength < 8 || passwordLength > 128){
+    alert("Password must be between 8 and 128 characters!")
+    Length()
+    }
+    else{
+      console.log(passwordLength + " characters for password")
+    }
+  }
+  Length()
+  
+   
+//want lowercase prompt
+function lowercase(){
+  var lower= confirm("Do you want lowercase letters?")
+  if(lower){
+    console.log("yes to lowercase")
+  }
+  else{
+    console.log("no to lowercase")
+
+  }
+  if(lower==true){
+    console.log("Thanks for picking lowercase!")
+  }
+  else{
+    console.log("should have picked lowercase");
+    
+  }
+}
+lowercase()
+
+
+//want uppercase prompt
+function uppercase(){
+  var uppercase= confirm("Do you want uppercase letters?")
+  if(uppercase){
+    console.log("yes to uppercase")
+  }
+  else{
+    console.log("no to uppsercase")
+
+  }
+}
+uppercase()
+
+//want number prompt
+function numbers(){
+  var numbers= confirm("Do you want numbers?")
+  if(numbers){
+    console.log("yes to numbers")
+  }
+  else{
+    console.log("no to numbers")
+
+  }
+}
+numbers()
+
+//want special characters prompt
+function special(){
+  var special= confirm("Do you want special characters?")
+  if(special){
+    console.log("yes to special characters")
+  }
+  else{
+    console.log("no to special characters")
+
+  }
+}
+special()
